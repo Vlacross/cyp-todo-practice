5878b322b8a49a8bf5a79ccc919edfbbf8b0b496
Tim Vlasuk <timvlasuk@gmail.com>
2019-10-28 13:00:05 -0700

    added basic functionality test for todo input and add-todo function

--- cypress/integration/AA-todo-implementaion.js
+++ cypress/integration/AA-todo-implementaion.js
@@ -38,29 +38,40 @@ describe('Todo page loads', function() {
     .should('have.prop', 'disabled', false)
     cy.get('.showActive')
     .should('have.prop', 'disabled', false)
-
-   
-    
   })
 
-  it.only('captures input value', function() {
+  it('label captures focus for input', function() {
     cy.visit('http://localhost:3000/')
 
     cy.get('.todo-input-label').click()
     cy.focused().should('have.prop', 'class', 'todo-input')
-    
   })
 
+  it('input captures value and adds todo', function() {
+    
+    cy.visit('http://localhost:3000/')
+
+    /*check length of list before operation */
+    cy.get('.todo-list').then(($ul) => {
+      expect($ul[0].children.length).to.equal(2)
+    })
+
+    /* check the input is accepting user value*/
+    cy.get('.todo-input').click()
+    .type('newToDoEh?')
+    .should('have.value', 'newToDoEh?')
+
+    /*add todo*/
+    cy.get('.todo-submit').click()
+
+    /*check the input is emptied*/
+    cy.get('.todo-input').click()
+    .should('have.value', '')
+    
+    /*check the length of the list after adding element*/
+    cy.get('.todo-list').then(($ul) => {
+      expect($ul[0].children.length).to.equal(3)
+    })
+  })
 
 })
-
-
-
-// cy.get(['.showAll', 'showActive', '.showCompleted'])
-// // cy.get('.showAll').to.have.attr('disabled')
-// cy.get('.showAll')
-// .should('have.prop', 'disabled', true)
-// cy.get('.showActive')
-// .should('have.prop', 'disabled', false)
-// cy.get('.showCompleted')
-// .should('have.prop', 'disabled', false)
